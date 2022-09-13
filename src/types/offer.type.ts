10343dc03e40642c6ea99a5eba850d1d1155f41c
Капутин Евгений Анатольевич <ea_kaputin@protek.ru>
2022-09-13 12:16:18 +0300

    1.16. Приложение для терминала

---
+++ src/types/offer.type.ts
@@ -0,0 +1,24 @@
+import { Convenience } from './сonvenience.type.js';
+import { OfferTypeEnum } from './offer-type.enum.js';
+import { User } from './user.type.js';
+import { OfferCityEnum} from './offer-city.enum.js';
+import { CoordinatesType } from './coordinates.type';
+
+export type Offer = {
+  title: string;
+  description: string;
+  postDate: Date;
+  city: OfferCityEnum;
+  previewImage: string;
+  image: string;
+  premium: boolean;
+  rating: number;
+  type: OfferTypeEnum;
+  roomsNumber: number;
+  guestsNumber: number;
+  price: number;
+  facilities: Convenience[];
+  user: User;
+  commentsNumber: number;
+  coordinates: CoordinatesType;
+}
